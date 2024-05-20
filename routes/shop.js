245bf3f08566f29d8e64f42c6935c41bb26735ea
unknown <trustoppof3@gmail.com>
2024-05-21 00:33:10 +0300

    p

--- routes/shop.js
+++ routes/shop.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 
 const shopController = require('../controllers/shop');
 
-router.get('/shop',shopController.getproducts);
+router.get('/',shopController.getproducts);
 
-router.get('/shop/:productId',shopController.getOneproduct);
+router.get('/:productId',shopController.getOneproduct);
 
 module.exports = router;
